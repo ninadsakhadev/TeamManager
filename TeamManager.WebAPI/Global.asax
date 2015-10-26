0fb1de8d3420dff24565c2a9487406dfdfc91e74
ninadsakhadev <ninadsakhadev@gmail.com>
2015-10-26 16:24:20 +0200

    Adding Missing Files

---
+++ TeamManager.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamManager.WebAPI.WebApiApplication" Language="C#" %>
---
+++ TeamManager.WebAPI/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using Newtonsoft.Json;
+using System.Net.Http.Formatting;
+
+namespace TeamManager.WebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            UnityConfig.RegisterComponents(); 
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+
+            //var xml = GlobalConfiguration.Configuration.Formatters.XmlFormatter;
+            //var dcs = new System.Runtime.Serialization.DataContractSerializer(typeof(Department), null, int.MaxValue,
+            //    false, /* preserveObjectReferences: */ true, null);
+            //xml.SetSerializer<Department>(dcs);
+            var json = new JsonMediaTypeFormatter();
+            //var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling =
+                Newtonsoft.Json.PreserveReferencesHandling.All;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(json);
+        }
+    }
+}
