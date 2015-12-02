2fb40a23b454d694dc3499a7b7f457ea4b051c1e
ninadsakhadev <ninadsakhadev@gmail.com>
2015-12-02 16:26:08 +0200

    Adding Constants project

---
+++ TeamManager.Constants/TeamManagerConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamManager.Constants
+{
+    public class TeamManagerConstants
+    {
+        public const string TeamManagerAPI = "http://localhost:1267/";
+        public const string TeamManagerClient = "https://localhost:44300/";
+        public const string IdSrvIssuerUri = "https://teammanageridsrv3/embedded";
+        public const string IdSrv = "https://localhost:44300/identity";
+        public const string IdSrvToken = IdSrv + "/connect/token";
+        public const string IdSrvAuthorize = IdSrv + "/connect/authorize";
+        //test
+    }
+}
